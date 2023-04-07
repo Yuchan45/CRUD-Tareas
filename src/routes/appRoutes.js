021869a4a5e013cf9b5e1f6bf1853477e7583b62
Yuchan <yu.nakasone@gmail.com>
2023-04-07 19:12:07 -0300

    Mas endpoints documentados. Queda averiguar como hacer para ponerlo en otro archivo porque asi esta horrible

--- src/routes/appRoutes.js
+++ src/routes/appRoutes.js
@@ -51,10 +51,55 @@ const validateUser = require('../middlewares/validateUserMiddleware');
  *          example:
  *              username: Tomas Yu Nakasone
  *              password: Hola123!
+ * 
+ *      Task:
+ *          type: object
+ *          properties:
+ *              id:
+ *                  type: integer
+ *                  description: Task id
+ *              title: 
+ *                  type: string
+ *                  description: Task title
+ *              description: 
+ *                  type: string
+ *                  description: Task description
+ *              completed:
+ *                  type: boolean
+ *                  description: Task status
+ *              creationDate: 
+ *                  type: string
+ *                  description: Task creation date
+ *              updatedDate: 
+ *                  type: string
+ *                  description: Task last updated date
+ *          example:
+ *              id: 01cdc3c1-ae78-4b0e-8907-7fcd2b41b46e
+ *              title: Tarea 1!
+ *              description: Soy la tarea n1
+ *              completed: false
+ *              creationDate: March 26th 2023, 7:59:01 pm
+ *              updatedDate: March 26th 2023, 8:00:44 pm
  */
 
 
 // User Routes
+/**
+ * @swagger
+ * /usuario:
+ *  get:
+ *      summary: Returns a user register form.
+ *      tags: [User]
+ *      responses:
+ *          200:
+ *              description: Successfull get request!
+ *              content: 
+ *                  'text/html':
+ *                  schema:
+ *                      
+ *                      
+ * 
+ */
 router.get('/usuario', register);
 /**
  * @swagger
@@ -72,6 +117,8 @@ router.get('/usuario', register);
  *      responses:
  *          200:
  *              description: New user created!
+ *          400:
+ *              description: Incorrect credentials!
  *      
  * 
  */
@@ -87,9 +134,48 @@ router.get('/protected', validateToken, getTasks); // Muestra las tareas (solo e
 router.get('/task', createTask); // Formulario de 'crear tarea'.
 router.post('/task', validateTask, processCreateTask);
 
+/**
+ * @swagger
+ * /tasks:
+ *  get:
+ *      summary: Returns all tasks.
+ *      tags: [Tasks]
+ *      responses:
+ *          200:
+ *              description: All tasks returned successfully!
+ *              content: 
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items: 
+ *                              $ref: '#/components/schemas/Task'
+ */
 router.get('/tasks', getTasks);
 
 router.delete('/task/:id', deleteTasks);
+
+/**
+ * @swagger
+ * /task/{id}:
+ * put:
+ *     summary: Sets the specif1ied task to state 'completed'
+ *     tags: [Tasks]
+ *     parameters:
+ *         - in: path
+ *         name: id
+ *         schema: 
+ *             type: string
+ *             required: true
+ *             description: The task id
+ *     responses:
+ *         200:
+ *             description: Task set to 'completed' successfully!
+ *             content: 
+ *                 application/json:
+ *                     schema:
+ *                         type: object
+ *                         $ref: '#/components/schemas/Task'
+ */
 router.put('/task/:id', completeTask);
 
 
